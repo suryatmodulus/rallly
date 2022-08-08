aa9fb6ebbe081abb4c778e56faf07f59633638bc
Luke Vella <me@lukevella.com>
2022-08-08 14:04:52 +0100

    Remove session from router

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -5,13 +5,11 @@ import { createContext } from "../../../server/context";
 import { createRouter } from "../../../server/createRouter";
 import { login } from "../../../server/routers/login";
 import { polls } from "../../../server/routers/polls";
-import { session } from "../../../server/routers/session";
 import { user } from "../../../server/routers/user";
 import { withSessionRoute } from "../../../utils/auth";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("session.", session)
   .merge("polls.", polls)
   .merge(login)
   .merge("user.", user);
